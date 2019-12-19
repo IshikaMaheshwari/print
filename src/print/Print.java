67020f26fdf2111edfaf66a37082056fb8d8f57e
IshikaMaheshwari <56021457+IshikaMaheshwari@users.noreply.github.com>
2019-12-19 11:49:23 +0530

    m1

--- src/print/Print.java
+++ src/print/Print.java
@@ -1,6 +1,7 @@
 package print;
 public class Print {
     public static void main(String[] args) {
-        System.out.println("Hi Git print changed");
+
+        System.out.println("Hi Git print changed message added");
     }
 }
